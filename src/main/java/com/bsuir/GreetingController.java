f00fe97e00f5561975f2a06aaa19af4e6f3784b5
Vitali <vital.dz@mail.ru>
2020-02-21 20:15:28 +0300

    Greeting testing

---
+++ src/main/java/com/bsuir/GreetingController.java
@@ -0,0 +1,22 @@
+package com.bsuir;
+
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    @PostMapping("/greeting")
+    public Greeting greetingName(@RequestBody Greeting greet) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, greet.getContent()));
+    }
+}
